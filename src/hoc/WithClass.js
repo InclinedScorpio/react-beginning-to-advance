c061b49ba7098849dfded1992647fcdafd4cf3ef
ashutoshtiwari3309 <ashutosh.tiwari@squareboat.com>
2020-05-28 15:07:20 +0530

    HOC Doubt Resolved {...props}

--- src/hoc/WithClass.js
+++ src/hoc/WithClass.js
@@ -3,7 +3,8 @@ import React from "react";
 const withClass = (WrappedComponent, classes) => {
 	return props => (
 		<div>
-			<WrappedComponent />
+			{console.log("123455", { ...props })}
+			<WrappedComponent {...props} />
 		</div>
 	);
 };
